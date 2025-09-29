91f93839171c62904c82b109ec2fa1e08387cac1
afsha9jabee9 <afsha9jabee9@gmail.com>
2025-09-28 23:07:19 -0500

    Delete models directory

--- models/models-players.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const playerSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  age: Number,
-  team: String,
-});
-
-module.exports = mongoose.model("Player", playerSchema);
